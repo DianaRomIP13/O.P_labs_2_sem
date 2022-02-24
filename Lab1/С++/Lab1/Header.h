41b22f12c6c50ed70bbdf86ee051c63b2dfdf32c
DianaRomIP13 <90607027+DianaRomIP13@users.noreply.github.com>
2022-02-24 03:13:26 +0200

    Lab1

---
+++ Lab1/С++/Lab1/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void inFile(string);
+void compare(string, string, string, int&);
+void outFile(string);
